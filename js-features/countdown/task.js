9abecb844f85d14f4662daebc0fd9b6c32c407bf
Anna <merkulova.anna96@gmail.com>
2023-06-27 15:37:56 +0300

    countdown done

---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+let secondsLeft = document.getElementById("timer");
+let timer = Number(secondsLeft.textContent);
+let timerId = null;
+
+function countdown() {
+    timer = timer - 1;
+    secondsLeft.textContent = timer;
+    if(timer <= 0){
+        clearInterval(timerId);
+        alert('Вы победили в конкурсе!');
+    }
+}
+
+timerId = setInterval(countdown, 50);
