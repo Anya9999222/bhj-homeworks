8e4f39abd063266e307b979d5fe47369fe624b71
Anna <merkulova.anna96@gmail.com>
2023-08-09 23:41:27 +0300

    mistakes corrected

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,9 +5,12 @@ element.classList.add("tooltip");
 tools.forEach((el,index) => {
     el.addEventListener('click', function(e){
         e.preventDefault()
+        let coordinate = el.getBoundingClientRect();
         element.textContent = tools[index].title;
-        tools[index].insertAdjacentElement("beforeend", element);
-        element.classList.add("tooltip_active");
+        tools[index].insertAdjacentElement("afterEnd", element);
+        element.style.top = String(coordinate.top + 20) + 'px';
+        element.style.left = String(coordinate.left) + 'px';
+        element.classList.toggle("tooltip_active");
     })
 })
 
