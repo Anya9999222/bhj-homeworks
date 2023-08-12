d2ae38f2d321307f13c440bd5a4f93cd6a9717fa
Anna <merkulova.anna96@gmail.com>
2023-08-12 12:57:43 +0300

    исправлены ошибки

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,18 +1,45 @@
-const tools = document.querySelectorAll(".has-tooltip");
-let element = document.createElement("div");
-element.classList.add("tooltip");
+document.addEventListener("click", function(e){
+    e.preventDefault();
+    let tooltip = e.target;
+    let nextEl = tooltip.nextElementSibling;
 
-tools.forEach((el,index) => {
-    el.addEventListener('click', function(e){
-        e.preventDefault()
-        let coordinate = el.getBoundingClientRect();
-        element.textContent = tools[index].title;
-        tools[index].insertAdjacentElement("afterEnd", element);
+    if(nextEl && nextEl.classList.contains('tooltip_active')){
+        nextEl.classList.remove("tooltip_active");
+        nextEl.remove()
+        return
+    }
+    if(tooltip.classList.contains("has-tooltip")){
+        let element = document.createElement("div");
+        element.classList.add("tooltip");
+        tooltip.insertAdjacentElement("afterEnd", element);
+        element.textContent = tooltip.title;
+        
+        let coordinate = tooltip.getBoundingClientRect();
         element.style.top = String(coordinate.top + 20) + 'px';
         element.style.left = String(coordinate.left) + 'px';
-        element.classList.toggle("tooltip_active");
-    })
+        element.classList.add("tooltip_active");
+    }
 })
 
+// const tools = document.querySelectorAll(".has-tooltip");
+// let element = document.createElement("div");
+// element.classList.add("tooltip");
 
-
+// tools.forEach((el,index) => {
+//     el.addEventListener('click', function(e){
+//         e.preventDefault()
+        
+//         if(element){
+//             element.classList.remove("tooltip_active")
+//         } else {
+           
+//         let coordinate = el.getBoundingClientRect();
+//         element.textContent = tools[index].title;
+//         tools[index].insertAdjacentElement("afterEnd", element);
+//         element.style.top = String(coordinate.top + 20) + 'px';
+//         element.style.left = String(coordinate.left) + 'px';
+//         element.classList.add("tooltip_active");
+//         }
+        
+//     })
+// })
