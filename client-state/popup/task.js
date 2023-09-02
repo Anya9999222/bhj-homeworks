7637d76bd851d78df42e58c63f8054f9c198e2e2
Anna <merkulova.anna96@gmail.com>
2023-09-02 21:23:50 +0300

    first commit

---
+++ client-state/popup/task.js
@@ -0,0 +1,21 @@
+const popup = document.getElementById('subscribe-modal');
+popup.classList.add('modal_active')
+getCookie('popup')
+
+
+function getCookie(name){
+    const cookies = document.cookie.split('; ')
+    console.log(cookies)
+    const item = cookies.find(p => p.startsWith(name))
+    if(item){
+        popup.classList.remove('modal_active')
+    }
+    
+}
+
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('modal__close')){
+        popup.classList.remove('modal_active')
+        document.cookie = "popup=close; expires=Tue, 19 Jan 2038 03:14:07 GMT;"
+    }
+})
