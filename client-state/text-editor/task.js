6d9c34adf093d4cccbb55beb0f1463ea32465eed
Anna <merkulova.anna96@gmail.com>
2023-09-08 14:30:27 +0300

    mistakes corrected

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,8 +1,6 @@
 let storedText = localStorage.getItem('textInEditor')
 
-if(storedText) {
-    document.getElementById('editor').innerHTML = storedText;
-}
+document.getElementById('editor').innerHTML = storedText;
 
 document.addEventListener('keyup', function(){
     localStorage.setItem('textInEditor', document.querySelector('#editor').value)
