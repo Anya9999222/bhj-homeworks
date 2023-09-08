646364e2fa579f16d958392c0470d74a91a05921
Anna <merkulova.anna96@gmail.com>
2023-09-08 14:24:00 +0300

    mistakes corr

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -3,28 +3,45 @@ const welcome = document.getElementById('welcome');
 const divForm = document.getElementById('signin');
 const spanId = document.getElementById('user_id');
 
-function welcomeGuest(){
-  divForm.classList.remove('signin_active')
-  welcome.classList.add('welcome_active')
-  
+
+function checkGuest(){
+  let authUser = localStorage.getItem('user');
+  if(authUser){
+    welcomeGuest(authUser);
+  }
+ 
 }
-form.addEventListener('submit', function(e){
+
+function welcomeGuest(id){
+  spanId.textContent = id;
+  form.reset();
+  divForm.classList.remove('signin_active')
+  welcome.classList.add('welcome_active')  
+}
+
+function sendData(e){
   e.preventDefault();
 
   const formData = new FormData(form);
   const request = new XMLHttpRequest();
+  
   request.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth')
+  request.responseType = 'json';
   request.send(formData);
-  request.onload = function () {
-    if(request.readyState === request.DONE){
-      let response = JSON.parse(request.response)
+
+  request.addEventListener('load', function () {
+      let response = request.response
       if(response.success === true){
         localStorage.setItem('user', response.user_id )
-        spanId.textContent = response.user_id
-        welcomeGuest()
+        user = response.user_id
+        welcomeGuest(user)
       }else{
         alert('Неверный логин/пароль')
       }
     }
-  } 
-})
+  ) 
+}
+
+
+form.addEventListener('submit', sendData)
+document.addEventListener('DOMContentLoaded',checkGuest)
